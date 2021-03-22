3c21ff098c757cd29b714f9f05498820b8fd03d7
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-03-23 02:00:52 +0300

    fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 
 const Form = () => {
   const inputTextHandler = (e) => {
-    console.log(e);
   };
-return (
   <form>
     <input onChange={inputTextHandler} type="text" className="todo-input" />
     <button className="todo-button" type="submit">
-      {/* <i className="fas fa-plus-square"> </i> */}
     </button>
     <div className="select">
       <select name="todos" className="filter-todo">
-        <option value="all">Все задачи</option>
-        <option value="uncompleted">Незавершенные</option>
+        <option value="all">Все задачи</option>     <option value="uncompleted">Незавершенные</option>
         <option value="completed">Завершенные</option>
       </select>
     </div>
-  </form>
-);
+  </form>;
 };
 
 export default Form;
